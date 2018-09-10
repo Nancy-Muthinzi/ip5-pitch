6b3674506bd81d10d1e752f018d7a8c3c19c02bf
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-09-10 10:36:13 +0300

    create password hashes for users

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class Pitch:
     '''
@@ -13,6 +14,8 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+
 
     def __repr__(self):
         return f'User {self.username}'
