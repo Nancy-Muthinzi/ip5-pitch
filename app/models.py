a802ccd13e7661898fd00345e544c73b8d51cd60
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-09-10 10:41:49 +0300

    create functions to generate and check password hashes

--- app/models.py
+++ app/models.py
@@ -16,9 +16,20 @@ class User(db.Model):
     username = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
 
+@property
+def password(self):
+    raise AttributeError('You cannot read the password attribute')
 
-    def __repr__(self):
-        return f'User {self.username}'
+@password.setter
+def password(self, password):
+    self.pass_secure = generate_password_hash(password)
+
+
+def verify_password(self,password):
+    return check_password_hash(self.pass_secure,password)
+
+def __repr__(self):
+    return f'User {self.username}'
 
 class Role(db.Model):
     __tablename__ = 'roles'
