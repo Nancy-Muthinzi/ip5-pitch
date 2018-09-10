ab6961cb27a1c67f114aa931816f5d31c41d3f37
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-09-10 12:15:21 +0300

    create auth routes for register, login & logout

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,7 @@ from flask import render_template
 from . import main
 # from .models import review
 # from .forms import ReviewForm
+from flask_login import login_required
 
 # Review = review.Review
 
@@ -17,7 +18,7 @@ def index():
     return render_template('index.html',title = title)
 
 
-@main.route('/pitch/<int:pitch_id>')
+@main.route('/pitch/<int:pitch_id>', methods = ['GET','POST'])
 def pitch(pitch_id):
 
     '''
@@ -25,17 +26,5 @@ def pitch(pitch_id):
     '''
     return render_template('pitch.html',id = pitch_id)    
 
-# @main.route('/pitch/review/new/<int:id>', methods = ['GET','POST'])
+# @login_required
 # def new_review(id):
-#     form = ReviewForm()
-#     movie = get_pitch(id)
-
-#     if form.validate_on_submit():
-#         title = form.title.data
-#         review = form.review.data
-#         new_review = Review(id, pitch, review)
-#         new_review.save_review()
-#         return redirect('pitch',id = pitch.id )
-
-#     title = f'{pitch.title} review'
-#     return render_template('new_review.html',title = title, review_form=form, pitch=pitch)    
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from . import db
+from . import db, login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 
 class Pitch:
     '''
@@ -10,10 +11,13 @@ class Pitch:
         self.id = id
         self.pitch = pitch
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    # password_hash = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
 
 @property
@@ -40,3 +44,7 @@ class Role(db.Model):
 
     def __repr__(self):
         return f'User {self.name}'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))        
