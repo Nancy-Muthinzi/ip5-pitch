9b69790eed9429631b57a89ce7c6b72ce17e6a16
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-09-11 16:46:53 +0300

    edit readme file

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa-student:ms@localhost/pitch'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
